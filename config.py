55d560a417cb3cd7efce75218e7b039b23ce3490
namgivu <nam.gi.vu@gigacover.com>
2019-11-05 12:52:19 +0700

    flexible DB_NAME

--- config.py
+++ config.py
@@ -6,7 +6,10 @@ from sqlalchemy.orm import sessionmaker, scoping
 DB_PORT=os.environ.get('DB_PORT')
 if not DB_PORT: DB_PORT = 5432
 
-db_uri = f'postgres://postgres:postgres@localhost:{DB_PORT}/postgres'
+DB_NAME=os.environ.get('DB_NAME')
+if not DB_NAME: DB_NAME = 'bxdoan_falcon'
+
+db_uri = f'postgres://postgres:postgres@localhost:{DB_PORT}/{DB_NAME}'
 engine = create_engine(db_uri)
 db = scoping.scoped_session(
             sessionmaker(bind = engine,
